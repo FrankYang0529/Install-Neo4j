07ba277bc99ed1446c6044d2f4494f106ebbbd16
Frank Yang <yangpoan@gmail.com>
2016-05-11 18:24:27 +0800

    update java to 8

--- install_neo4j.sh
+++ install_neo4j.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java7-installer
+sudo apt-get install oracle-java8-installer
 wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 echo 'deb http://debian.neo4j.org/repo stable/' >/tmp/neo4j.list
 sudo mv /tmp/neo4j.list /etc/apt/sources.list.d
